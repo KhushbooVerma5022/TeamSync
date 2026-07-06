8f52029c3857bb9f1dcc99342f82556035ee3f02
Khushboo Verma <khushbooverma8447@gmail.com>
2026-07-07 00:29:17 +0530

    Setup backend architecture and health check API

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import healthRoute from './routes/health.routes';
+import { errorHandler } from "./middleware/error.middleware";
 
 const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-export default app;
+app.use('/api/v1/health', healthRoute);
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        "success": false,
+        "message": "Route Not Found"
+    })
+})
+
+app.use(errorHandler);
+
+export default app;        
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,28 @@
+import dotenv from 'dotenv';
 import app from './app';
+import connectDB from './config/db';
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
+const PORT = process.env.PORT;
 
-const server = app.listen(PORT);
+if (!PORT) {
+    throw new Error('PORT is not defined')
+}
 
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`)
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server is running successfully on port ${PORT}`)
+        })
+
+    } catch (error) {
+        console.error('Failed to start server', error);
+        process.exit(1);
+    }
+};
+
+startServer();
+
+
